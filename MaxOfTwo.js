7a6b82c33716e0e8428d2fb2d26cba5b3e1c494d
William <Floydfamily71@gmail.com>
2022-02-19 18:32:09 -0700

    initial commit

---
+++ MaxOfTwo.js
@@ -0,0 +1,15 @@
+x = 2;
+y = 2;
+
+
+if(x == y){
+    console.log("The values are identical.");
+}
+else{
+    if (x > y){
+        console.log(x);
+    }
+    else{
+        console.log(y);
+    }
+}
